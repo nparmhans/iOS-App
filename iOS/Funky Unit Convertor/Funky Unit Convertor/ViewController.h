913410569997ee225b2e32a9cab1384b6c490feb
Nirupa Parmhans <nirjoon89@gmail.com>
2014-10-10 21:35:45 -0400

    Initial Commit

---
+++ iOS/Funky Unit Convertor/Funky Unit Convertor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Funky Unit Convertor
+//
+//  Created by Nirupa Parmhans on 10/10/14.
+//  Copyright (c) 2014 Nirupa Parmhans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS/iOS/Funky Unit Converter/Funky Unit Converter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Funky Unit Converter
+//
+//  Created by Nirupa Parmhans on 10/10/14.
+//  Copyright (c) 2014 Nirupa Parmhans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
