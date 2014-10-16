68d8753c172d4fa5e8d5546862937a67b3c9f281
Nirupa Parmhans <nirjoon89@gmail.com>
2014-10-16 16:52:30 -0400

    Hooking up the View Objects

--- Postcard/Postcard/ViewController.swift
+++ Postcard/Postcard/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func sendNowButton(sender: UIButton)
+    {
+        
+        
+    }
 
 }
 
