e1490acf00a9556d5c3e06898a237217bd4e2062
Nirupa Parmhans <nirjoon89@gmail.com>
2014-11-04 19:45:06 -0500

    Adding Multiple Dogs

--- Man's BestFriend/Man's BestFriend/ViewController.m
+++ Man's BestFriend/Man's BestFriend/ViewController.m
@@ -25,9 +25,26 @@
     myDog.breed = @"St. Bernard";
     myDog.age = 1;
     myDog.image = [UIImage imageNamed:@"stBernard.jpg"];
+    
+    MBFDog *secondDog = [[MBFDog alloc] init ];
+    secondDog.name = @"Wishbone";
+    secondDog.breed = @"Jack Russell Terrier ";
+    secondDog.image = [UIImage imageNamed:@"JRT.jpg"];
+    
+    MBFDog* thirdDog = [[MBFDog alloc]init];
+    thirdDog.name = @"Lassie";
+    thirdDog.breed = @"Collie";
+    thirdDog.image = [UIImage imageNamed:@"BorderCollie.jpg"];
     self.myImageView.image = myDog.image;
     self.breedLabel.text = myDog.breed;
     self.nameLabel.text = myDog.name;
+    
+    MBFDog*fourthDog = [[MBFDog alloc] init];
+    fourthDog.name = @"Angel";
+    fourthDog.breed = @"Greyhound";
+    fourthDog.image = [UIImage imageNamed:@"ItalianGreyhound.jpg"];
+    
+    
     
 }
 
